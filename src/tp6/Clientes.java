6a94251833faa1574fae7fc71ebc91b902e78e5b
Melany Alcaraz <anto-04@hotmail.com.ar>
2022-05-09 21:46:31 -0300

    Initial commit

---
+++ src/tp6/Clientes.java
@@ -0,0 +1,72 @@
+package tp6;
+
+public class Clientes {
+
+    private long dni;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String ciudad;
+
+    private String domicilio;
+
+    public Clientes(long dni, String nombre, String apellido, String ciudad, String domicilio) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+        this.domicilio = domicilio;
+    }
+
+    public Clientes() {
+    }
+
+   
+
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCiudad(String cuidad) {
+        this.ciudad = cuidad;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public long getDni() {
+        return dni;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Clientes{" + "dni= " + dni + ", nombre= " + nombre + ", apellido= " + apellido + ", cuidad= " + ciudad + ", domicilio= " + domicilio + '}';
+    }
+
+    
+   
+}
---
+++ src/tp6/DirTelefonico.java
@@ -0,0 +1,61 @@
+package tp6;
+
+import java.util.*;
+
+
+public class DirTelefonico {
+
+    TreeMap<Long,Clientes> directorio = new TreeMap <>();
+
+    public boolean agregarCliente(long telefono, Clientes cliente) {
+        if(!directorio.containsKey(telefono)){
+             directorio.put(telefono,cliente);
+             return true;
+             
+        } else {
+            return false;
+        }
+    }
+
+    public Clientes buscarCliente(long telefono) {
+       // System.out.println("Se encontro el cliente: ");
+        return directorio.get(telefono);
+
+    }
+
+    public List<Long> buscarTelefono(Clientes apellido) {
+        ArrayList<Long> telefonos = new ArrayList<>();
+        //obtiene la clave
+        Set<Long> claves = directorio.keySet();
+        Iterator<Long> it= claves.iterator();
+        while(it.hasNext()){
+            Long telefono= it.next();
+            Clientes c= directorio.get(telefono);
+            if(c.getApellido() .equals(apellido)){
+                telefonos.add(telefono);
+            }}
+        return telefonos;
+    }
+
+    public List<Clientes> buscarClientes(String ciudad){
+        
+        ArrayList<Clientes> clientes = new ArrayList<>();
+        
+        Set<Long> tels = directorio.keySet();
+        Iterator<Long> it = tels.iterator();
+        while(it.hasNext()){
+            Long telefono = it.next();
+            Clientes c = directorio.get(telefono);
+            if(c.getCiudad().equals(ciudad)){
+                clientes.add(c);
+            }
+        }
+        return clientes;
+    }
+
+
+    public Clientes borrarCliente(long telefono) {
+        Clientes cliente= directorio.remove(telefono);
+        return cliente;
+    }
+}
